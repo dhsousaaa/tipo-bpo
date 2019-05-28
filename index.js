f90d7346c7d8bfb977dd4910cb5fcc1f9567870a
Diego Sousa <32400677+dhsousaaa@users.noreply.github.com>
2019-05-28 17:26:01 -0300

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+//App token: 38322148-ed98-328b-b605-7bbea07ff14e
+//Secret: 8d01baae-9928-3aee-8298-0b3c759568c2
+//Access token: 1e8ac12b-8abb-30c7-8653-6fcb9448893e (válido apenas para esta licença: diegosousa)
